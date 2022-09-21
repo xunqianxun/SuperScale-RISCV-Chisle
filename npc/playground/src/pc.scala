3dce1a2bb6ebdce0da87b197b9a1c6bbca6c568e
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-21 16:27:47 +0800

    >  generate verilog ysyx_22040228 Li Guo Qi Linux guoqili-xps-13 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:27:47 up 2 days,  7:48,  1 user,  load average: 0.67, 0.51, 0.54

---
+++ npc/playground/src/pc.scala
@@ -0,0 +1,23 @@
+import chisel3._
+
+class pc extends Module{
+    val io = IO(nwe Bundle {
+        val fect_addr       = Input(UInt(64.W))
+        val pc_stall        = Input(Bool())
+        val idrb1_addr      = Input(UInt(64.W))
+        val idrb1_ena       = Input(Bool())
+        val idrb2_addr      = Input(UInt(64.W))
+        val idrb2_ena       = Input(Bool())
+        val pc              = Output(UInt(64.W))
+    })
+
+    val Pc_reg = RegInit("x80000000".U(64.W))
+
+    when(idrb1_ena)       {Pc_reg := idrb1_addr}
+    .elsewhen(idrb2_ena)  {Pc_reg := idrb2_addr}
+    .elsewhen(pc_stall)   {Pc_reg := Pc_reg    }  
+    .otherwise            {Pc_reg := fect_addr }
+    
+    io.pc := Pc_reg
+    
+}
