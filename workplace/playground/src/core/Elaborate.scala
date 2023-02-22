c7c335ebab7c927cc286ba7eff275b5011ad2e8b
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-22 22:22:19 +0800

    >  generate verilog ysyx_22040228 Li Guo Qi Linux guoqili-xps-13 5.19.0-32-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 30 17:03:34 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:22:19 up  1:11,  1 user,  load average: 1.09, 0.89, 0.50

--- workplace/playground/src/core/Elaborate.scala
+++ workplace/playground/src/core/Elaborate.scala
@@ -1,7 +1,9 @@
+package core
+
 import circt.stage._
 
 object Elaborate extends App {
-  def top = new GCD()
+  def top = new CoreAlu()
   val useMFC = true // use MLIR-based firrtl compiler
   val generator = Seq(chisel3.stage.ChiselGeneratorAnnotation(() => top))
   if (useMFC) {
